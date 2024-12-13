c211320d82d797d71c873ea572a2eed5eae664c5
Biko Pougala <bpougala@gmail.com>
2024-12-13 10:02:54 +0000

    test

--- livetiming/streaming.go
+++ livetiming/streaming.go
@@ -157,7 +157,7 @@ func CreateTimingSubscribeMessage() SubscribeMessage {
 
 func CreateOriginalSessionMessage() SubscribeMessage {
 	var topics []string
-	topics = append(topics, "SessionInfo", "SessionData", "TimingData", "TimingStats", "TimingAppData", "LapCount", "DriverList")
+	topics = append(topics, "SessionInfo", "SessionData", "TimingData", "TimingStats", "TimingAppData", "LapCount")
 	var topicsList [][]string
 	topicsList = append(topicsList, topics)
 	return SubscribeMessage{
